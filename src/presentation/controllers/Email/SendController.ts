8e6fda361775b132fdb7f89031e8319f0ccbb6d1
williambrito98 <williambrito199850@gmail.com>
2023-07-15 21:45:47 -0300

    refactor: create helper method to missing params

--- src/presentation/controllers/Email/SendController.ts
+++ src/presentation/controllers/Email/SendController.ts
@@ -1,50 +1,26 @@
+import { badRequest } from '../../../helpers/httpHelpers'
 import { MissingParamError } from '../../errors/MissingParamError'
 import { type HttpResponse, type HttpRequest } from './../../protocols/http'
 export class SendController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.to?.length) {
-      return {
-        statusCode: 400,
-        body: {
-          message: new MissingParamError('to')
-        }
-      }
+      return badRequest(new MissingParamError('to'))
     }
 
     if (!httpRequest.body.from) {
-      return {
-        statusCode: 400,
-        body: {
-          message: new MissingParamError('from')
-        }
-      }
+      return badRequest(new MissingParamError('from'))
     }
 
     if (!httpRequest.body.subject) {
-      return {
-        statusCode: 400,
-        body: {
-          message: new MissingParamError('subject')
-        }
-      }
+      return badRequest(new MissingParamError('subject'))
     }
 
     if (!httpRequest.body.fields) {
-      return {
-        statusCode: 400,
-        body: {
-          message: new MissingParamError('fields')
-        }
-      }
+      return badRequest(new MissingParamError('fields'))
     }
 
     if (!httpRequest.body.type_body) {
-      return {
-        statusCode: 400,
-        body: {
-          message: new MissingParamError('type_body')
-        }
-      }
+      return badRequest(new MissingParamError('type_body'))
     }
 
     return {
