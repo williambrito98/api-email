2554729b7ba4daa7ab2f862e58909ef1d7f2f3f1
williambrito98 <williambrito199850@gmail.com>
2023-07-15 21:50:01 -0300

    refactor: move validate fields to loop

--- src/presentation/controllers/Email/SendController.ts
+++ src/presentation/controllers/Email/SendController.ts
@@ -3,24 +3,11 @@ import { MissingParamError } from '../../errors/MissingParamError'
 import { type HttpResponse, type HttpRequest } from './../../protocols/http'
 export class SendController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.to?.length) {
-      return badRequest(new MissingParamError('to'))
-    }
-
-    if (!httpRequest.body.from) {
-      return badRequest(new MissingParamError('from'))
-    }
-
-    if (!httpRequest.body.subject) {
-      return badRequest(new MissingParamError('subject'))
-    }
-
-    if (!httpRequest.body.fields) {
-      return badRequest(new MissingParamError('fields'))
-    }
-
-    if (!httpRequest.body.type_body) {
-      return badRequest(new MissingParamError('type_body'))
+    const requiredFields = ['to', 'from', 'subject', 'fields', 'type_body']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
     return {
