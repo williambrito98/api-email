59fcebb1ba0340b8fd96b8f3213bf337e4280d6c
williambrito98 <williambrito199850@gmail.com>
2023-07-15 21:31:54 -0300

    refactor: ensure SendController implements correct interfaces

--- src/presentation/controllers/Email/SendController.ts
+++ src/presentation/controllers/Email/SendController.ts
@@ -1,40 +1,54 @@
+import { type HttpResponse, type HttpRequest } from './../../protocols/http'
 export class SendController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.to?.length) {
       return {
         statusCode: 400,
-        message: new Error('Missing param: to')
+        body: {
+          message: new Error('Missing param: to')
+        }
       }
     }
 
     if (!httpRequest.body.from) {
       return {
         statusCode: 400,
-        message: new Error('Missing param: from')
+        body: {
+          message: new Error('Missing param: from')
+        }
       }
     }
 
     if (!httpRequest.body.subject) {
       return {
         statusCode: 400,
-        message: new Error('Missing param: subject')
+        body: {
+          message: new Error('Missing param: subject')
+        }
       }
     }
 
     if (!httpRequest.body.fields) {
       return {
         statusCode: 400,
-        message: new Error('Missing param: fields')
+        body: {
+          message: new Error('Missing param: fields')
+        }
       }
     }
 
     if (!httpRequest.body.type_body) {
       return {
         statusCode: 400,
-        message: new Error('Missing param: type_body')
+        body: {
+          message: new Error('Missing param: type_body')
+        }
       }
     }
 
-    return {}
+    return {
+      body: '',
+      statusCode: 200
+    }
   }
 }
