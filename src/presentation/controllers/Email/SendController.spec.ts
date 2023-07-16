fb760f8b6093f8b074e4c3943f79df63a8ddbebe
williambrito98 <williambrito199850@gmail.com>
2023-07-15 21:08:15 -0300

    test: ensure SendController return an error if no receiver is provided

---
+++ src/presentation/controllers/Email/SendController.spec.ts
@@ -0,0 +1,35 @@
+/*
+    {
+        to: string[],
+        from: string,
+        subject: string
+        fields: {
+            variable_1: string
+            variable_2: string
+            ....
+        }
+        type_body: string
+    }
+*/
+
+import { SendController } from './SendController'
+
+describe('Email: Send Controller', () => {
+  test('Should return 400 if no receiver is provided', () => {
+    const sut = new SendController()
+    const httpRequest = {
+      body: {
+        from: 'email@mai.com',
+        subject: 'subject',
+        type_body: 'success',
+        fields: {
+          name: 'name',
+          data: 'data'
+        }
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.message).toEqual(new Error('Missing param: to'))
+  })
+})
